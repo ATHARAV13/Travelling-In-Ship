102d4e8e5b59a60c47681c934dfc81c23752e067
ATHARAV13 <87124276+ATHARAV13@users.noreply.github.com>
2021-07-24 16:28:09 +0530

    Add files via upload

---
+++ c11.js
@@ -0,0 +1,24 @@
+var seaImg,sea;
+var shipImg,ship;
+
+function preload(){
+seaImg=loadImage("sea.png");
+shipImg=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4,png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX= -3;
+  
+  ship=createSprite(120,200,30,30);
+  ship.addAnimation("running",shipImg);
+}
+
+function draw() {
+  background("blue");
+ if(sea.x<0){
+   sea.x=sea.width/2;
+ }
+}
